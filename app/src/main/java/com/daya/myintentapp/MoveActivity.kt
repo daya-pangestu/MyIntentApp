c0494688753d747f132db4edf95eb08c01142792
dayfrk <day.pangestu@gmail.com>
2019-08-02 20:36:03 +0700

    intent

---
+++ app/src/main/java/com/daya/myintentapp/MoveActivity.kt
@@ -0,0 +1,14 @@
+package com.daya.myintentapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MoveActivity : AppCompatActivity(){
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_move)
+    }
+
+}
